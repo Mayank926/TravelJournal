a6072e8505fb1d24ba6838654d7797cb68669383
Mayank Madhav <mayank.madhav@hotmail.com>
2025-05-02 20:22:49 +0530

    Hotfix for vite config revert base

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <header>
             <div className="header-container">
-                <img src = "/images/globe.png" alt="Globe Icon"></img>
+                <img src = "images/globe.png" alt="Globe Icon"></img>
                 <span>my travel journal</span>
             </div>
         </header>
